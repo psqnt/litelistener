6b5c9e2785c3f245294fa3f95bbd85e1d36b3a5a
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2026-01-05 15:51:02 -0500

    working poll func

--- src/litelistener/listener.py
+++ src/litelistener/listener.py
@@ -1,7 +1,43 @@
 from time import sleep
 from liteclient.client import LiteClient
 
-POLL_INTERVAL = 1
+POLL_INTERVAL = 2
+
+
+def poll(
+    handlers: dict,
+    host: str,
+    port: int,
+    target_keys: list,
+    poll_interval: int = POLL_INTERVAL,
+):
+    """
+    Blocks and listens for data. When data is found, it calls 'callback(key, data)'.
+    """
+    client = LiteClient(host, port)
+    print(f"[*] Listener active. Watching: {target_keys}")
+
+    try:
+        while True:
+            # RPOP returns [key_name, value]
+            for key in target_keys:
+                result = client.rpop(key)
+
+                if result:
+                    # Execute the external logic passed into this function
+                    handler = handlers.get(key)
+                    if handler:
+                        handler(result)
+                    else:
+                        print(f"[?] No handler registered for key: {key}")
+                else:
+                    print("no result")
+            sleep(poll_interval)
+
+    except KeyboardInterrupt:
+        print("\n[*] Shutting down...")
+    finally:
+        client.close()
 
 
 # This only works if the server (litequeue) implements a blpop command handler
@@ -27,34 +63,6 @@ def listen(handlers: dict, host: str, port: int, target_keys: list):
                     handler(data)
                 else:
                     print(f"[?] No handler registered for key: {key_name}")
-    except KeyboardInterrupt:
-        print("\n[*] Shutting down...")
-    finally:
-        client.close()
-
-
-def poll(handlers: dict, host: str, port: int, target_keys: list):
-    """
-    Blocks and listens for data. When data is found, it calls 'callback(key, data)'.
-    """
-    client = LiteClient(host, port)
-    print(f"[*] Listener active. Watching: {target_keys}")
-
-    try:
-        while True:
-            # BLPOP returns [key_name, value]
-            for key in target_keys:
-                result = client.rpop(target_keys)
-                if result:
-                    key_name, data = result
-                    # Execute the external logic passed into this function
-                    handler = handlers.get(key_name)
-                    if handler:
-                        handler(data)
-                    else:
-                        print(f"[?] No handler registered for key: {key_name}")
-            sleep(POLL_INTERVAL)
-
     except KeyboardInterrupt:
         print("\n[*] Shutting down...")
     finally:
